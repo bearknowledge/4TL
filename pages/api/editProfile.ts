7e2f6c77fd62d480c2fca9b115acb63de73966df
Zach Miller <home@mac-2.local>
2022-11-26 22:41:22 -0500

    return redirect

--- pages/api/editProfile.ts
+++ pages/api/editProfile.ts
@@ -25,24 +25,29 @@ export default async function handler(req: any, res: any) {
     } else if (req.body.telegram != undefined) {
       await db.collection("accounts").updateOne({token: req.cookies.Autherized}, {
         $set: {telegram: req.body.telegram}})
+        res.redirect("/profile/" + rString)
   
     } else if (req.body.email != undefined) {
       await db.collection("accounts").updateOne({token: req.cookies.Autherized}, {
         $set: {email: req.body.email}})
+        res.redirect("/profile/" + rString)
 
     } else if (req.body.company != undefined) {
       await db.collection("accounts").updateOne({token: req.cookies.Autherized}, {
         $set: {company: req.body.company}})
+        res.redirect("/profile/" + rString)
 
 
     } else if (req.body.industry != undefined) {
       await db.collection("accounts").updateOne({token: req.cookies.Autherized}, {
         $set: {industry: req.body.industry}})
+        res.redirect("/profile/" + rString)
 
 
     } else if (req.body.linkedin != undefined) {
       await db.collection("accounts").updateOne({token: req.cookies.Autherized}, {
         $set: {linkedin: req.body.linkedin}})
+        res.redirect("/profile/" + rString)
     } 
 
   }
